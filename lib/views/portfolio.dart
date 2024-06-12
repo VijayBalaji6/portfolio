939ca281a269a2f542c967afedfa9eb62731af82
= <=>
2024-06-12 16:45:26 +0530

    added mobile view

--- lib/views/portfolio.dart
+++ lib/views/portfolio.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:portfolio/constants/app_gloabls.dart';
 import 'package:portfolio/views/app_bar/portfolio_floating_app_bar.dart';
+import 'package:portfolio/views/drawer/app_bar_drawer.dart';
 import 'package:portfolio/views/screens/about_view/about.dart';
 import 'package:portfolio/views/screens/contacts_view/contacts.dart';
-import 'package:portfolio/views/screens/home_view/home.dart';
-import 'package:portfolio/views/screens/work_view/work.dart';
+import 'package:portfolio/views/screens/home_view/home_view.dart';
+import 'package:portfolio/views/screens/work_view/work_view.dart';
 
 class PortFolio extends StatelessWidget {
   const PortFolio({super.key});
@@ -13,6 +14,8 @@ class PortFolio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: drawerKey,
+      drawer: const AppBarDrawer(),
       appBar: const PortFolioFloatingAppBar(),
       body: SingleChildScrollView(
         physics: const AlwaysScrollableScrollPhysics(),
